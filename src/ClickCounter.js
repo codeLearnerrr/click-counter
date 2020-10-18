3aed6cd415d88c4a2c4d003d50c55182201c3bb9
Jeferson Eiji <jeferson.eiji@icloud.com>
2020-10-17 22:51:32 -0300

    added Icons

--- src/ClickCounter.js
+++ src/ClickCounter.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './ClickCounter.css';
-
+import './assets/icomoon/style.css'
 function ClickCounter() {
 
     const [count, setCount] = useState(0);
@@ -10,8 +10,8 @@ function ClickCounter() {
             <h1 className="welcomeHeader">Welcome to the Click Counter App:</h1>
             <h2 className="clicksDisplay">The current clicks are <br/> {count} </h2>
             <div className="buttons">
-                <button onClick={() => setCount(count + 1)} id="buttonIncrease" type="button">+</button>
-                <button onClick={() => setCount(count - 1)} id="buttonDecrease" type="button">-</button>
+                <button onClick={() => setCount(count + 1)} id="buttonIncrease" type="button" className="icon-addIcon-min"></button>
+                <button onClick={() => setCount(count - 1)} id="buttonDecrease" type="button" className="icon-subtractIcon-min" ></button>
             </div>
         </div>
     )
