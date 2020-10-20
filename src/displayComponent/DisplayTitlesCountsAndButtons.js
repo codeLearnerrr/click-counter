30efa95fcfc258ddea1b91dfb59e731057f3c4a2
Jeferson Eiji <jeferson.eiji@icloud.com>
2020-10-20 19:11:25 -0300

    added sound

--- src/displayComponent/DisplayTitlesCountsAndButtons.js
+++ src/displayComponent/DisplayTitlesCountsAndButtons.js
@@ -1,6 +1,16 @@
 import React, { useState } from 'react';
 import '.././displayComponent/DisplayTitlesCountsAndButtons.css';
 import '../assets/buttonIcons/style.css';
+import clickAddSound from '../assets/sounds/clickAddSound.wav';
+import clickSubtractSound from '../assets/sounds/clickSubtractSound.wav';
+
+
+const playAddClick = () => {
+    document.getElementById('add').play();
+};
+const playSubtractClick = () => {
+    document.getElementById('subtract').play();
+};
 
 function DisplayTitlesCountsAndButtons() {
 
@@ -11,9 +21,11 @@ function DisplayTitlesCountsAndButtons() {
             <h1 className="headerTitle">Welcome to the Click Counter App:</h1>
             <h2 className="countsTitle">The current clicks are <br /> {count} </h2>
             <div className="buttons">
-                <button onClick={() => setCount(count + 1)} type="button" className="icon-addIcon-min buttonStyle"></button>
-                <button onClick={() => setCount(count - 1)} type="button" className="icon-subtractIcon-min buttonStyle" ></button>
+                <button onClick={() => { setCount(count + 1); playAddClick(); }} type="button" className="icon-addIcon-min buttonStyle"></button>
+                <button onClick={() => { setCount(count - 1); playSubtractClick(); }} type="button" className="icon-subtractIcon-min buttonStyle" ></button>
             </div>
+            <audio id="add" src={clickAddSound}></audio>
+            <audio id="subtract" src={clickSubtractSound}></audio>
         </div>
     )
 }
